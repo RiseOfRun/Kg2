cdabe250d69fb8a133295017887e02f139b6969a
RiseOfRun <runi54rus@gmail.com>
2020-03-30 19:20:33 +0700

    many new shit

--- Kg2/Source.cpp
+++ Kg2/Source.cpp
@@ -1,156 +1,267 @@
+п»ї
 #include <stdlib.h>
 #include <iostream>
-#include <fstream>
 #include <vector>	
 #include <GL/freeglut.h>
-#include <math.h>
-#include <windows.h>
-#include <GL/glut.h>
+#include <glm/mat4x4.hpp>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#define KEY_ESCAPE 27
 using namespace std;
-class Triangle
+
+typedef glm::vec3 vec;
+
+float rotate_x = 30, rotate_y = 30;
+
+class Primal
 {
 public:
-	vector <GLfloat> x;
-	vector <GLfloat> y;
-	vector <GLfloat> z;
-	Triangle(vector <GLfloat> x1, vector <GLfloat> y1, vector <GLfloat> z1)
+	vector<vec> points;
+	vec norm;
+	Primal(vector<vec> points)
 	{
-		x = x1;
-		y = y1;
-		z = z1;
+		this->points = points;
 	}
-	Triangle() {};
-	~Triangle() {};
+	Primal() {
+
+	}
+	~Primal() {
+
+	}
 };
 
-vector <GLfloat> Norm(vector <GLfloat> x, vector <GLfloat> y, vector <GLfloat> z)
+class Edition
 {
-	vector<GLfloat> v = { y[0]-x[0], y[1] - x[1], y[2] - x[2] };
-	vector<GLfloat> w = { z[0] - x[0], z[1] - x[1], z[2] - x[2] };
+public:
+    vec position;
 
-	vector<GLfloat> norm = {
-		v[1] * w[2] - v[2] * w[1],
-		v[2] * w[0] - v[0] * w[2],
-		v[0] * w[1] - w[1] * v[0]
-	};
-	return norm;
+};
+
+typedef struct {
+	int width;
+	int height;
+	char* title;
+
+	float field_of_view_angle;
+	float z_near;
+	float z_far;
+} glutWindow;
+
+glutWindow win;
+
+
+void display() {
+    GLfloat params[16];
+
+    //  Clear screen and Z-buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    float f[16];
+    // Reset transformations
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glGetFloatv(GL_MODELVIEW_MATRIX, params);
+    glm::mat4 matrix = glm::make_mat4(params);
+   
+    vec test;
+    test = { 1,2,3 };
+    float* test2;
+    test2 = glm::value_ptr(test);
+    matrix = glm::rotate(matrix,glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glMultMatrixf(glm::value_ptr(matrix));
+    //float* test = glm::value_ptr(test);
+   // glPushMatrix();
+
+    //glBegin(GL_POLYGON);
+
+    //glColor3f(1.0, 0.0, 0.0);     glVertex3f(0.5, -0.5, -0.5);      // P1 is red
+    //glColor3f(0.0, 1.0, 0.0);     glVertex3f(0.5, 0.5, -0.5);      // P2 is green
+    //glColor3f(0.0, 0.0, 1.0);     glVertex3f(-0.5, 0.5, -0.5);      // P3 is blue
+    //glColor3f(1.0, 0.0, 1.0);     glVertex3f(-0.5, -0.5, -0.5);      // P4 is purple
+
+    //glEnd();
+
+    //// White side - BACK
+    //glBegin(GL_POLYGON);
+    //glColor3f(1.0, 1.0, 1.0);
+    //glVertex3f(0.5, -0.5, 0.5);
+    //glVertex3f(0.5, 0.5, 0.5);
+    //glVertex3f(-0.5, 0.5, 0.5);
+    //glVertex3f(-0.5, -0.5, 0.5);
+    //glEnd();
+
+    //// Purple side - RIGHT
+    //glBegin(GL_POLYGON);
+    //glColor3f(1.0, 0.0, 1.0);
+    //glVertex3f(0.5, -0.5, -0.5);
+    //glVertex3f(0.5, 0.5, -0.5);
+    //glVertex3f(0.5, 0.5, 0.5);
+    //glVertex3f(0.5, -0.5, 0.5);
+    //glEnd();
+
+    //// Green side - LEFT
+    //glBegin(GL_POLYGON);
+    //glColor3f(0.0, 1.0, 0.0);
+    //glVertex3f(-0.5, -0.5, 0.5);
+    //glVertex3f(-0.5, 0.5, 0.5);
+    //glVertex3f(-0.5, 0.5, -0.5);
+    //glVertex3f(-0.5, -0.5, -0.5);
+    //glEnd();
+
+    //// Blue side - TOP
+    //glBegin(GL_POLYGON);
+    //glColor3f(0.0, 0.0, 1.0);
+    //glVertex3f(0.5, 0.5, 0.5);
+    //glVertex3f(0.5, 0.5, -0.5);
+    //glVertex3f(-0.5, 0.5, -0.5);
+    //glVertex3f(-0.5, 0.5, 0.5);
+    //glEnd();
+    //// Red side - BOTTOM
+    //glBegin(GL_POLYGON);
+    //glColor3f(1.0, 0.0, 0.0);
+    //glVertex3f(0.5, -0.5, -0.5);
+    //glVertex3f(0.5, -0.5, 0.5);
+    //glVertex3f(-0.5, -0.5, 0.5);
+    //glVertex3f(-0.5, -0.5, -0.5);
+    //glEnd();
+
+    // Other Transformations
+    // glTranslatef( 0.1, 0.0, 0.0 );      // Not included
+    // glRotatef( 180, 0.0, 1.0, 0.0 );    // Not included
+
+    // Rotate when user changes rotate_x and rotate_y
+    //glGetFloatv(GL_MODELVIEW_MATRIX, params);
+    //glRotatef(rotate_x, 1.0, 0.0, 0.0);
+    //glGetFloatv(GL_MODELVIEW_MATRIX, params);
+    //glRotatef(rotate_y, 0.0, 1.0, 0.0);
+    //glGetFloatv(GL_MODELVIEW_MATRIX, params);
+
+
+    // Other Transformations
+    // glScalef( 2.0, 2.0, 0.0 );          // Not included
+
+   //Multi-colored side - FRONT
+    glBegin(GL_POLYGON);
+
+    glColor3f(1.0, 0.0, 0.0);     glVertex3f(0.5, -0.5, -0.5);      // P1 is red
+    glColor3f(0.0, 1.0, 0.0);     glVertex3f(0.5, 0.5, -0.5);      // P2 is green
+    glColor3f(0.0, 0.0, 1.0);     glVertex3f(-0.5, 0.5, -0.5);      // P3 is blue
+    glColor3f(1.0, 0.0, 1.0);     glVertex3f(-0.5, -0.5, -0.5);      // P4 is purple
+
+    glEnd();
+
+
+    // White side - BACK
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex3f(0.5, -0.5, 0.5);
+    glVertex3f(0.5, 0.5, 0.5);
+    glVertex3f(-0.5, 0.5, 0.5);
+    glVertex3f(-0.5, -0.5, 0.5);
+    glEnd();
+
+    // Purple side - RIGHT
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f(0.5, -0.5, -0.5);
+    glVertex3f(0.5, 0.5, -0.5);
+    glVertex3f(0.5, 0.5, 0.5);
+    glVertex3f(0.5, -0.5, 0.5);
+    glEnd();
+
+    // Green side - LEFT
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(-0.5, -0.5, 0.5);
+    glVertex3f(-0.5, 0.5, 0.5);
+    glVertex3f(-0.5, 0.5, -0.5);
+    glVertex3f(-0.5, -0.5, -0.5);
+    glEnd();
+
+    // Blue side - TOP
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.5, 0.5, 0.5);
+    glVertex3f(0.5, 0.5, -0.5);
+    glVertex3f(-0.5, 0.5, -0.5);
+    glVertex3f(-0.5, 0.5, 0.5);
+    glEnd();
+    // Red side - BOTTOM
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(0.5, -0.5, -0.5);
+    glVertex3f(0.5, -0.5, 0.5);
+    glVertex3f(-0.5, -0.5, 0.5);
+    glVertex3f(-0.5, -0.5, -0.5);
+    glEnd();
+
+  
+    glFlush();
+    glutSwapBuffers();
+
 }
-//class Vector3f
-//{
-//public:
-//	float x, y, z;
-//
-//	Vector3f() {};							//конструктор по умолчанию
-//	Vector3f(float _x, float _y, float _z)	//конструктор
-//	{
-//		x = _x;
-//		y = _y;
-//		z = _z;
-//	}
-//
-//	//Переопределение оператора +
-//	Vector3f operator + (Vector3f _Vector)
-//	{
-//		return Vector3f(_Vector.x + x, _Vector.y + y, _Vector.z + z);
-//	}
-//	//Переопределение оператора -
-//	Vector3f operator - (Vector3f _Vector)
-//	{
-//		return Vector3f(x - _Vector.x, y - _Vector.y, z - _Vector.z);
-//	}
-//
-//	//Переопределение оператора *
-//	Vector3f operator * (float num)
-//	{
-//		return Vector3f(x * num, y * num, z * num);
-//	}
-//
-//	//Переопределение оператора /
-//	Vector3f operator / (float num)
-//	{
-//		return Vector3f(x / num, y / num, z / num);
-//	}
-//};
-Triangle * wannaDraw;
-bool drawing = false;
-GLint Width = 512, Height = 512;
-vector<GLfloat> CurrentCol = { 1,0,0 };
-int currentPolygonIndex = -1;
-/*Функция обрабатывет действия мыши*/
-/*Функция вывода на экран*/
-void Display(void)
+
+
+void initialize()
 {
-
-	glClearColor(1, 1, 1, 0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	int left = (Width - 200) / 2;   // левая граница
-	int right = left + 200;          // правая граница
-	int bottom = (Height - 200) / 2;  // нижняя граница
-	int top = bottom + 200;        // верхняя граница
-	glColor3ub(0, 255, 0);
-	for (float i = -50; i <= 50; i += 1)
-	{
-		glBegin(GL_LINES);
-		// Ось Х
-		glVertex3f(-50, 0, i);
-		glVertex3f(50, 0, i);
-
-		// Ось Z
-		glVertex3f(i, 0, -50);
-		glVertex3f(i, 0, 50);
-		glEnd();
-	}
-	//glBegin(GL_TRIANGLES);
-	//vector<GLfloat> norm = Norm(wannaDraw->x, wannaDraw->y, wannaDraw->z);
-	////glNormal3f(norm[0],norm[1],norm[2]);
-	//glColor3ub(0, 255, 0);
-	//glVertex3f(100, 200,0);
-	//glVertex3f(200,200,0);
-	//glVertex3f(200,100,0);
-	//glEnd;
-	glBegin(GL_TRIANGLES);
-	glColor3ub(255, 0, 0);
-	glVertex3f(100,200,0);	glVertex3f(200,200,-50);
-	glVertex3f(200,100,0);
-	glEnd();
-
-	glFinish();
+	glMatrixMode(GL_PROJECTION);	// select projection matrix
+	//glViewport(0, 0, win.width, win.height);									// set the viewport	
+	glLoadIdentity();															// reset projection matrix
+	GLfloat aspect = (GLfloat)win.width / win.height;
+	glOrtho(-10,10,-10,10, 1000,3);
+	//gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);		
+	glTranslatef(0.0f, 0.0f, -3.0f);
+	// set up a perspective projection matrix
+	glMatrixMode(GL_MODELVIEW);													// specify which matrix is the current matrix
+	//glShadeModel(GL_SMOOTH);
+	//glClearDepth(1.0f);														// specify the clear value for the depth buffer
+	//glEnable(GL_DEPTH_TEST);
+	//glDepthFunc(GL_LEQUAL);
+	//glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);						// specify implementation-specific hints
+	glClearColor(1.0, 1.0, 1.0, 1.0);											// specify clear values for the color buffers								
 }
 
-/* Функция вызывается при изменении размеров окна */
-void Reshape(GLint w, GLint h)
-{
-	/*Отображаемый цвет*/
-	Width = w; Height = h;
-	/* устанавливаем размеры области отображения */
-	glViewport(0, 0, w, h);
-	/* ортографическая проекция */
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, w, 0, h, -1.0, 1.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glutPostRedisplay();
+
+void specialKeys(int key, int x, int y) {
+
+    //  Right arrow - increase rotation by 5 degree
+    if (key == GLUT_KEY_RIGHT)
+        rotate_y += 5;
+
+    //  Left arrow - decrease rotation by 5 degree
+    else if (key == GLUT_KEY_LEFT)
+        rotate_y -= 5;
+
+    else if (key == GLUT_KEY_UP)
+        rotate_x += 5;
+
+    else if (key == GLUT_KEY_DOWN)
+        rotate_x -= 5;
+
+    //  Request display update
+    glutPostRedisplay();
+
 }
 
-/* Функция обрабатывает сообщения c клавиатуры */
-void Keyboard(unsigned char key, int x, int y)
+int main(int argc, char** argv)
 {
-#define ESCAPE '\033'
-	if (key == ESCAPE) exit(0);
-	
-}
+	// set window values
+	win.width = 640;
+	win.height = 480;
+	win.field_of_view_angle = 45;
+	win.z_near = 1.0f;
+	win.z_far = 500.0f;
 
-void main(int argc, char* argv[])
-{
-	wannaDraw = new Triangle({0,0,0}, {0,255,0}, {255,255,0});
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB);
-	glutInitWindowSize(Width, Height);
-	glutCreateWindow("glut");
-	glutDisplayFunc(Display);
-	glutReshapeFunc(Reshape);
-	glutKeyboardFunc(Keyboard);
-	glutMainLoop();
+	// initialize and run program
+	glutInit(&argc, argv);                                      // GLUT initialization
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);  // Display Mode
+	glutInitWindowSize(win.width, win.height);					// set window size
+	glutCreateWindow(win.title);								// create Window
+    glEnable(GL_DEPTH_TEST);
+	glutDisplayFunc(display);									// register Display Function
+	glutIdleFunc(display);									// register Idle Function
+    glutSpecialFunc(specialKeys);							// register Keyboard Handler
+	initialize();
+	glutMainLoop();												// run GLUT mainloop
+	return 0;
 }
